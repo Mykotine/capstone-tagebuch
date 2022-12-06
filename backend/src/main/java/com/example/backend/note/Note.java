4a389d7a25c70d820ca74c0ef7b12cc83d0e6e29
mykola <bobirets@gmail.com>
2022-12-06 16:51:03 +0100

    fix mongodb connection

--- backend/src/main/java/com/example/backend/note/Note.java
+++ backend/src/main/java/com/example/backend/note/Note.java
@@ -4,17 +4,12 @@ package com.example.backend.note;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
-
-
 
 @Data
 public class Note{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String text;
     private String tag;
 
