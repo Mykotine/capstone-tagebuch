2e06efbdd9f067946a8fb75614311bdfe13991dd
mykola <bobirets@gmail.com>
2022-12-09 23:31:03 +0100

    add UnitTests

--- backend/src/main/java/com/example/backend/sevice/UserService.java
+++ backend/src/main/java/com/example/backend/sevice/UserService.java
@@ -1,10 +1,13 @@
 package com.example.backend.sevice;
 
 
+import com.example.backend.domain.Role;
 import com.example.backend.domain.User;
 import com.example.backend.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +18,17 @@ public class UserService {
         return userRepository.findByUsername(username);
     }
 
-    public User saveUser(User user){
-        return userRepository.save(user);
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if(userFromDb != null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+
+        userRepository.save(user);
+
+        return true;
     }
 }
